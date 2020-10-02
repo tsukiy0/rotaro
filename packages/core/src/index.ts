a3ac86252016951e51cb5c810346063d38f4410a
tsukiy0 <tsukiy0@users.noreply.github.com>
2020-10-02 20:42:47 +1000

    feat: export models

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,3 +1,7 @@
-export const sum = (a: number, b: number): number => {
-  return a + b;
-};
+export * from "./models/Days";
+export * from "./models/Person";
+export * from "./models/PersonDays";
+export * from "./models/PersonList";
+export * from "./models/PersonRotation";
+export * from "./models/Roster";
+export * from "./models/Schedule";
