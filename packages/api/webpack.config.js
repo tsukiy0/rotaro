4bef9c1dd70238e5dc122e7e7b2612a72815fd74
tsukiy0 <tsukiy0@users.noreply.github.com>
2020-10-03 11:53:05 +1000

    ci: build api bundle for aws lambda runtime

---
+++ packages/api/webpack.config.js
@@ -0,0 +1,33 @@
+const path = require("path");
+
+module.exports = {
+  entry: path.join(__dirname, "src/index.ts"),
+  output: {
+    path: path.join(__dirname, "out"),
+    filename: "bundle.js",
+    library: "index",
+    libraryTarget: "commonjs2",
+  },
+  target: "node",
+  mode: "production",
+  devtool: "eval",
+  resolve: {
+    extensions: [".js", ".ts"],
+  },
+  optimization: {
+    minimize: false,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: {
+          loader: "ts-loader",
+          options: {
+            configFile: "tsconfig.build.json",
+          },
+        },
+      },
+    ],
+  },
+};
