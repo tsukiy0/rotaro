8dfe24f6c70dbaf279873b4d0a560af41cbc3400
tsukiy0 <tsukiy0@users.noreply.github.com>
2020-10-03 21:59:24 +1000

    feat: deploy dynamodb

--- packages/deployment/src/RootStack.ts
+++ packages/deployment/src/RootStack.ts
@@ -1,10 +1,13 @@
 import { Stack, Construct, StackProps } from "@aws-cdk/core";
+import { DatabaseConstruct } from "./constructs/DatabaseConstruct";
 import { OutputConstruct } from "./constructs/OutputConstruct";
 import { WebConstruct } from "./constructs/WebConstruct";
 
 export class RootStack extends Stack {
   constructor(scope: Construct, id: string, props: StackProps) {
     super(scope, id, props);
+
+    new DatabaseConstruct(this, "Database");
 
     const web = new WebConstruct(this, "Web");
 
---
+++ packages/deployment/src/constructs/DatabaseConstruct.ts
@@ -0,0 +1,40 @@
+import { Construct, RemovalPolicy } from "@aws-cdk/core";
+import {
+  Table,
+  BillingMode,
+  Attribute,
+  AttributeType,
+  ProjectionType,
+} from "@aws-cdk/aws-dynamodb";
+
+export class DatabaseConstruct extends Construct {
+  public readonly table: Table;
+
+  constructor(scope: Construct, id: string) {
+    super(scope, id);
+
+    const idAttribute: Attribute = {
+      name: "id",
+      type: AttributeType.STRING,
+    };
+
+    const scheduleHourAttribute: Attribute = {
+      name: "scheduleHour",
+      type: AttributeType.STRING,
+    };
+
+    const table = new Table(this, "Table", {
+      partitionKey: idAttribute,
+      billingMode: BillingMode.PAY_PER_REQUEST,
+    });
+
+    table.addGlobalSecondaryIndex({
+      indexName: "scheduleHour_id",
+      partitionKey: scheduleHourAttribute,
+      sortKey: idAttribute,
+      projectionType: ProjectionType.ALL,
+    });
+
+    this.table = table;
+  }
+}
