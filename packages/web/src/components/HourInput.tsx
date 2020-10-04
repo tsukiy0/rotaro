e1ea47cceec6be350a5810cd7710bf52235f36f1
tsukiy0 <tsukiy0@users.noreply.github.com>
2020-10-04 13:35:32 +1100

    feat: add HourInput

---
+++ packages/web/src/components/HourInput.tsx
@@ -0,0 +1,29 @@
+import { FormControl, FormLabel, Select } from "@chakra-ui/core";
+import { Hour, hourFromString } from "@rotaro/core";
+import React from "react";
+import { BaseProps } from "../models/BaseProps";
+
+export const HourInput: React.FC<BaseProps<{
+  value: Hour;
+  onChange: (value: Hour) => void;
+}>> = ({ className, value, onChange }) => {
+  return (
+    <FormControl className={className}>
+      <FormLabel htmlFor="hour">Hour</FormLabel>
+      <Select
+        name="hour"
+        placeholder="Select option"
+        value={value}
+        onChange={(e) => onChange(hourFromString(e.target.value))}
+      >
+        {Object.values(Hour).map((_) => {
+          return (
+            <option key={_} value={_}>
+              {_}
+            </option>
+          );
+        })}
+      </Select>
+    </FormControl>
+  );
+};
