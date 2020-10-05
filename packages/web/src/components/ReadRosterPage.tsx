863b4896cb320d8ef4a0c563146a1ab8e700e4f0
tsukiy0 <tsukiy0@users.noreply.github.com>
2020-10-05 16:22:05 +1100

    feat: display rotation

--- packages/web/src/components/ReadRosterPage.tsx
+++ packages/web/src/components/ReadRosterPage.tsx
@@ -1,4 +1,4 @@
-import { Box, useTheme } from "@chakra-ui/core";
+import { Box, Heading, useTheme } from "@chakra-ui/core";
 import { Roster, RosterId } from "@rotaro/core";
 import React, { useEffect, useState } from "react";
 import { useAlert } from "../contexts/AlertContext/AlertContext";
@@ -6,7 +6,6 @@ import { useServices } from "../contexts/ServicesContext/ServicesContext";
 import { BaseProps } from "../models/BaseProps";
 import { Card } from "./Card";
 import { LoadingPage } from "./LoadingPage";
-import { PersonListView } from "./PersonListView";
 
 export const ReadRosterPage: React.FC<BaseProps<{
   rosterId: RosterId;
@@ -36,9 +35,29 @@ export const ReadRosterPage: React.FC<BaseProps<{
 
   return (
     <Box className={className} padding={theme.space[4]}>
-      <Card>
-        <PersonListView personList={roster.rotation.personList} />
-      </Card>
+      {roster.rotation.rotation.map((_, i) => {
+        return (
+          <Card key={i}>
+            <Box
+              display="flex"
+              flexDirection="row"
+              justifyContent="space-between"
+              alignItems="center"
+            >
+              <Heading as="h1" size="md">
+                {
+                  roster.rotation.personList.items.find((person) =>
+                    person.id.equals(_.personId),
+                  )?.name
+                }
+              </Heading>
+              <Heading as="h2" size="sm">
+                {_.days.toNumber()}
+              </Heading>
+            </Box>
+          </Card>
+        );
+      })}
     </Box>
   );
 };
